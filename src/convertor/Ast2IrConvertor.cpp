bc50c8968b15998dcaffe53b72b3ad17bd53f275
FlyingYu-Z <zhengyuzyyou@gmail.com>
2023-01-07 14:07:16 +0800

    update

--- src/convertor/Ast2IrConvertor.cpp
+++ src/convertor/Ast2IrConvertor.cpp
@@ -28,28 +28,26 @@ Ast2IrConvertor::Ast2IrConvertor(FlyScriptParser::FileContext *fileContext) {
     }
 }
 
-string_view Ast2IrConvertor::typeContextToString(FlyScriptParser::TypeContext *typeContext){
-    auto typeValue=dynamic_cast<TerminalNodeImpl *>(typeContext->children[0])->symbol->getType();
-    return ValueType::getName(typeValue);
-}
-
-
 FileClass Ast2IrConvertor::generateFileClass() {
     FileClass fileClass = FileClass();
-    auto rawFieldList = vector<FlyScriptParser::FieldContext>();
-    auto rawStatList = vector<FlyScriptParser::StatContext>();
-    auto rawFunctionList = vector<FlyScriptParser::FunctionContext>();
+    auto rawFieldList = vector<FlyScriptParser::FieldContext *>();
+    auto rawStatList = vector<FlyScriptParser::StatContext *>();
+    auto rawFunctionList = vector<FlyScriptParser::FunctionContext *>();
 
-    for (int i=0;i<tree->children.size();i++) {
-        ParseTree *childTree=tree->children[i];
-        if(FlyScriptParser::FunctionContext* functionContext = dynamic_cast<FlyScriptParser::FunctionContext*>(childTree)) {
-            FlyScriptParser::TypeContext *returnType= dynamic_cast<FlyScriptParser::TypeContext *>(functionContext->children[0]);
-            string_view typeString= Ast2IrConvertor::typeContextToString(returnType);
-            printf("return type:%s",typeString.data());
-        }else{
-            if(FlyScriptParser::FieldContext* fieldContext = dynamic_cast<FlyScriptParser::FieldContext*>(childTree)) {
+    for (int i = 0; i < tree->children.size(); i++) {
+        ParseTree *childTree = tree->children[i];
+        if (FlyScriptParser::FunctionContext *functionContext = dynamic_cast<FlyScriptParser::FunctionContext *>(childTree)) {
+            FlyScriptParser::TypeContext *returnType = dynamic_cast<FlyScriptParser::TypeContext *>(functionContext->children[0]);
+            string_view typeString = Ast2IrConvertor::typeContextToString(returnType);
+            printf("return type:%s", typeString.data());
 
+            rawFunctionList.push_back(functionContext);
+        } else {
+            if (FlyScriptParser::FieldContext *fieldContext = dynamic_cast<FlyScriptParser::FieldContext *>(childTree)) {
+
+                rawFieldList.push_back(fieldContext);
             }
+            rawStatList.push_back(dynamic_cast<FlyScriptParser::StatContext *>(childTree));
         }
     }
 
@@ -58,4 +56,9 @@ FileClass Ast2IrConvertor::generateFileClass() {
 
 Ast2IrConvertor::~Ast2IrConvertor() {
 
+}
+
+string_view Ast2IrConvertor::typeContextToString(FlyScriptParser::TypeContext *typeContext) {
+    auto typeValue = dynamic_cast<TerminalNodeImpl *>(typeContext->children[0])->symbol->getType();
+    return ValueType::getName(typeValue);
 }
